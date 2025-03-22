66dac13e1a4eafe1e3e9fb7cbc854174d7527c0e
Long <vanlong5436@gmail.com>
2025-03-23 04:47:42 +0700

    bump database version to 2 (this will update database when user updates library)

--- ketch/src/main/java/com/ketch/internal/database/DownloadDatabase.kt
+++ ketch/src/main/java/com/ketch/internal/database/DownloadDatabase.kt
@@ -3,7 +3,7 @@ package com.ketch.internal.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [DownloadEntity::class], version = 1)
+@Database(entities = [DownloadEntity::class], version = 2)
 internal abstract class DownloadDatabase : RoomDatabase() {
     abstract fun downloadDao(): DownloadDao
 }
